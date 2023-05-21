8af939551615594f54127f1092a8225edd9d2741
NorichikaSugii <wawa60379@gmail.com>
2023-05-21 15:53:03 +0900

    Initial Commit

---
+++ 7DaysCallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7DaysCallenge
+//
+//  Created by 杉井位次 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
