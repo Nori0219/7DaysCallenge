a2575df40ea023f24e77b636600f0578f54e9ca4
NorichikaSugii <wawa60379@gmail.com>
2023-05-23 13:34:31 +0900

    :art: HomeViewControllerの画面配置

---
+++ 7DaysCallenge/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  7DaysCallenge
+//
+//  Created by 杉井位次 on 2023/05/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
