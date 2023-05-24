af5cc8fe6955a315bf81c2eb0ce891233b770f1f
NorichikaSugii <wawa60379@gmail.com>
2023-05-24 13:52:12 +0900

    :art: NewArticleViewControllerの画面配置

---
+++ 7DaysCallenge/NewArticleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewArticleViewController.swift
+//  7DaysCallenge
+//
+//  Created by 杉井位次 on 2023/05/24.
+//
+
+import UIKit
+
+class NewArticleViewController: UIViewController {
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var contextTextView: UITextView!
+    @IBOutlet var articleImage: UIImageView!
+    @IBOutlet var saveButon: UIButton!
+    @IBOutlet var imageButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
