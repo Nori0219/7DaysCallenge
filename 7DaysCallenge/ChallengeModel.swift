36d7efe9a3d79024968696880552e393681b9eda
NorichikaSugii <wawa60379@gmail.com>
2023-05-25 23:58:56 +0900

    :bug:　challengeModelの型修正

--- 7DaysCallenge/ChallengeModel.swift
+++ 7DaysCallenge/ChallengeModel.swift
@@ -13,35 +13,23 @@ import RealmSwift
 
 class Challenge: Object {
     //@Persisted(primaryKey: true) var UID = UUID().uuidString
-    @Persisted var title: String = ""
-    @Persisted var toDo: String = ""
-    @Persisted var startDate: Date = Date()
-    @Persisted var doNotification: Bool = false
-    @Persisted var streak: Int = 0
+    @Persisted var title: String = "" // 習慣のタイトル
+    @Persisted var toDo: String = "" // 実施すること
+    @Persisted var startDate: Date = Date() // 開始日
+    @Persisted var doNotification: Bool = false // 通知の有無
+    @Persisted var notificationTime: Date? // 通知時間
+    @Persisted var streak: Int = 0// Articleの個数
+    
+    // Relationship: ChallengeとArticleの関連付け
     let articles = List<Article>()
 }
 
 class Article: Object {
-    @Persisted var context: String = ""
-    var image: UIImage?
-    @Persisted var date: Date = Date()
-    //@Persisted(primaryKey: true) var id = UUID().uuidString
+    @Persisted var context: String = ""// 内容
+    @Persisted var imageData: Data? // 画像（Data型）
+    @Persisted var date: Date = Date()// 日付
+    
+    // Relationship: ArticleとChallengeの関連付け
+    let parentChallenge = LinkingObjects(fromType: Challenge.self, property: "articles")
 }
 
-
-//class Challenge {
-//    //@Persisted(primaryKey: true) var UID = UUID().uuidString
-//     var title: String = ""
-//    var toDo: String = ""
-//    var startDate: Date = Date()
-//    var doNotification: Bool = false
-//    var streak: Int = 0
-//    let articles = List<Article>()
-//}
-//
-//class Article {
-//     var context: String = ""
-//    var image: UIImage?
-//    var date: Date = Date()
-//    //@Persisted(primaryKey: true) var id = UUID().uuidString
-//}
