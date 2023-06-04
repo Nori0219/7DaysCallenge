658f1810170972f43ffdda6bd87eb426d36dd000
NorichikaSugii <wawa60379@gmail.com>
2023-06-04 18:59:24 +0900

    :sparkles: 通知メッセージの変更

--- 7DaysCallenge/Message.swift
+++ 7DaysCallenge/Message.swift
@@ -22,9 +22,14 @@ struct ChallengeMessage {
         case 4:
             return "すごい！連続\(challenge.streak)日間の偉業です。あなたはチャレンジのプロですね！"
         case 5:
-            return "ワオ！もう止められません！連続\(challenge.streak)日間の快挙です！次は何ができるか楽しみですね！"
+            return "ワオ！もう止められません！連続\(challenge.streak)日間の快挙です！目指せ、世界新記録！"
+        case 6:
+            return "\(challenge.toDo)\nなんとついに連続\(challenge.streak)日間も継続できました！いよいよ今日で最終日、ファイト🔥！"
+        case 7:
+            return "すごい！連続\(challenge.streak)日間の偉業です。あなたはチャレンジのプロですね！"
+            
         default:
-            return "順調ですね！連続\(challenge.streak)日目です。続けましょう！"
+            return "順調ですね！連続\(challenge.streak)日目です。もっと続けましょう！"
         }
     }
 }
