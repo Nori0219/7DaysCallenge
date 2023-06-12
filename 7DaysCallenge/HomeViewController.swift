999433c137add103bcb4ccd5e2f37773e1f49933
NorichikaSugii <wawa60379@gmail.com>
2023-06-12 19:45:45 +0900

    :sparkles: Homeにテストバナー広告追加

--- 7DaysCallenge/HomeViewController.swift
+++ 7DaysCallenge/HomeViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import GoogleMobileAds
 
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -14,10 +15,11 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     let realm = try! Realm()
     
     var topChallenge: Challenge?
-
+    
     @IBOutlet var messageTextView: UITextView!
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var bannerView: GADBannerView!
     
     
     override func viewDidLoad() {
@@ -47,6 +49,13 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
                 print("有効な通知リクエストはありません")
             }
         }
+        
+        // GADBannerViewのプロパティを設定
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        
+        // 広告読み込み
+        bannerView.load(GADRequest())
         
     }
     
